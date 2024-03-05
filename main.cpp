821742844a5a9d9a1cc355f81b95c33e6d38d185
MaxKlee23 <154516286+MaxKlee23@users.noreply.github.com>
2024-03-05 17:37:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,25 @@
-#include <QApplication>
-#include "chessboardwidget.h"
+#include <QCoreApplication>
+#include "simpleServer.h"
+#include "simpleClient.h"
+#include "spiel.h"
+#include "board.h"
+#include <iostream>
 
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+   QCoreApplication a(argc, argv);
+   Server server;
+   Client client;
+   client.start("127.0.0.1",8888);
+
+    spiel meinSpiel;
+    meinSpiel.startSpiel();
 
 
+    //meinBrett.printStartposition();
 
-    ChessboardWidget widget;
-    widget.show();
+    meinSpiel.endSpiel();
 
-    return app.exec();
-
-
+ //   return a.exec();
 }
